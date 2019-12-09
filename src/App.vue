2b0d44f08c50906941aeb104b0a21c8a448b6708
chin <woodichin@outlook.com>
2019-12-09 22:34:57 +0800

    feat(demo): flip demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="previous">previous</button>
+    <button @click="next">next</button>
+    <transition-group name="flip" class="flipWrapper">
+      <div v-for="i in list" :key='i' v-show="i === current" class="item">{{i}}</div>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      list: [0, 1,2,3,4,5],
+      current: 1
+    }
+  },
+  methods: {
+    previous() {
+      this.current = (this.current - 1 + this.list.length) % this.list.length
+    },
+    next() {
+      this.current = (this.current + 1) % this.list.length
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.flipWrapper {
+  position: absolute;
+  width: 100%;
+  top: 80px;
+  left: 0;
+  right: 0
+
+}
+.item {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+}
+.flip-enter-active, .flip-leave-active {
+  transition: all 1s;
+}
+.flip-enter{
+  transform: translateX(-100%)
+}
+.flip-enter-to {
+  transform: translateX(0)
+}
+.flip-leave {
+  transform: translateX(0) translateY(-60px)
+}
+.flip-leave-to {
+  transform: translateX(100%)
 }
 </style>
